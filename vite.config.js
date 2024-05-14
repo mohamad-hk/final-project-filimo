d605d9287d7149cf39bc159374cf194f5f6faafb
mohammad hossein karimi <hm.karimi.23179@gmail.com>
2024-05-15 01:01:31 +0330

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/filimo_final/"
+  base: "/final-project-filimo/"
 })
